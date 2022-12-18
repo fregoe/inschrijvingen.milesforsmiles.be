30bae1cb2e4f933f49e29b7b483d0a5ceedf93e9
Frederiek Goemaere <frederiek.goemaere@gocos.be>
2022-12-18 09:12:06 +0100

    feat: laste changes

--- config/constants.php
+++ config/constants.php
@@ -3,11 +3,11 @@
 return [
     'price' => 35,
     'from_mail' => 'admin@milesforsmiles.be',
-    'from_subject_deelnemer' => 'Bedankt voor uw inschrijving voor Miles for Smiles 2022',
-    'from_subject_inschrijver' => 'Bedankt voor uw inschrijvingen voor Miles for Smiles 2022',
+    'from_subject_deelnemer' => 'Bedankt voor uw inschrijving voor Miles for Smiles 2023',
+    'from_subject_inschrijver' => 'Bedankt voor uw inschrijvingen voor Miles for Smiles 2023',
     'from_subject_canceledorder' => 'Annulatie onafgewerkte inschrijving',
-    'from_subject_finaleinfo' => 'Praktische informatie voor Miles For Smiles.',
-    'from_subject_noteam' => 'U behoort nog tot geen team bij Miles for Smiles 2020',
+    'from_subject_finaleinfo' => 'Praktische informatie voor Miles For Smiles 2023.',
+    'from_subject_noteam' => 'U behoort nog tot geen team bij Miles for Smiles 2023',
     'goede_doelen' => [
         '1' => 'De Doorzetters',
         '2' => 'De Kade',
